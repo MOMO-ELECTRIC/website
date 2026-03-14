78df6b29f48548bad4dba15b99390209687f3261
Yixiao Wang <yixiao@Yixiaos-MBP.localdomain>
2026-03-14 12:52:48 -0700

    Update Chinese header subtitle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const i18n = {
   zh: {
-    brandTag: '执照电力承包商 · Southern California',
+    brandTag: '执照电力承包服务商 · Southern California',
     navServices: '首页',
     navRebate: '免费电箱升级',
     navContact: '联系我们',
